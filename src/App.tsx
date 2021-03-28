38b0bbb679f530b60470a0035d493a469ad86ed4
Jonas Hauser <info@jhauser.ch>
2021-03-28 21:57:52 +0200

    chore(setup): commitlint, commitizen and small stuff

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import { ThemeProvider } from '@material-ui/styles'
 import React from 'react'
 
 function App() {
-  return (
-    <ThemeProvider theme={createMuiTheme({ typography: { fontFamily: '"Poppins", sans-serif' } })}>
-      <CssBaseline />
-      <div>Hello World!</div>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider theme={createMuiTheme({ typography: { fontFamily: '"Poppins", sans-serif' } })}>
+            <CssBaseline />
+            <div>Hello World!</div>
+        </ThemeProvider>
+    )
 }
 
 export default App
