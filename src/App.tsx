5980a8ea9bfbbdd01f4f8ba2069997314e0587f3
Rafael Fuhrer <rafael.fuhrer@ost.ch>
2021-03-29 15:22:24 +0200

    feat(appbar): add appbar with search and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { CssBaseline } from '@material-ui/core'
 import { createMuiTheme } from '@material-ui/core/styles'
 import { ThemeProvider } from '@material-ui/styles'
+
 import React from 'react'
+import SearchAppBar from './components/AppBar'
 
 function App() {
-    return (
-        <ThemeProvider theme={createMuiTheme({ typography: { fontFamily: '"Poppins", sans-serif' } })}>
-            <CssBaseline />
-            <div>Hello World!</div>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider theme={createMuiTheme({ typography: { fontFamily: '"Poppins", sans-serif' } })}>
+      <CssBaseline />
+      <SearchAppBar />
+    </ThemeProvider>
+  )
 }
 
 export default App
