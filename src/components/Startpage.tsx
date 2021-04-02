0245bb66f4befd39a54f71e5d435d08574c33dcb
Jonas Hauser <jonas.hauser@ost.ch>
2021-04-02 13:24:23 +0200

    feat(stores): fetch cards from backend /stores endpoint

--- src/components/Startpage.tsx
+++ src/components/Startpage.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import StoreCard from './StoreCard';
 import { Grid } from '@material-ui/core';
+import { getStores } from '../services/storesService'
 
 export default function Startpage() {
-  const sampleCard = {
-    name: 'Migros St. Gallen',
-    maxCapacity: 180,
-    currentCapacity: 70
-  };
+    // TODO Optimize type (Array<any>)
+  const [stores, setStores] = useState<Array<any>>([]);
+
+  useEffect( () => {
+    const fetchStores = async () => {
+      setStores(await getStores());
+    };
+    fetchStores();
+  }, []);
+
+  // TODO Implement loading (loading spinner) and error state (error screen)
+
   return (
     <div className="content">
       <Grid container>
         <Grid item xs={12} md={7} sm={7} lg={5}>
+          {stores.map(store => (
           <StoreCard
-            name={sampleCard.name}
-            currentCapacity={sampleCard.currentCapacity}
-            maxCapacity={sampleCard.maxCapacity}
+              key={store.id}
+            name={store.name}
+            currentCapacity={store.currentCapacity}
+            maxCapacity={store.maxCapacity}
           />
+          ))}
         </Grid>
       </Grid>
     </div>
