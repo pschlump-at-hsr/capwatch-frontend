d66450ccb63e66550d7a571f24ef700fa5af4d60
Jonas Hauser <jonas.hauser@ost.ch>
2021-04-15 11:36:58 +0200

    feat(loading): add isLoading and hasError states

--- src/components/Startpage.tsx
+++ src/components/Startpage.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import StoreCard from './StoreCard';
-import { Grid } from '@material-ui/core';
+import { CircularProgress, Grid } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 import { getStores } from '../services/storesService';
 
 export default function Startpage() {
   // TODO Optimize type (Array<any>)
   const [stores, setStores] = useState<Array<any>>([]);
+
+  const isLoading = false;
+  const hasError = false;
+
+  const isEmpty = isLoading || hasError || stores.length < 1;
 
   useEffect(() => {
     const fetchStores = async () => {
@@ -25,22 +31,35 @@ export default function Startpage() {
     }
   }
 
-  // TODO Implement loading (loading spinner) and error state (error screen)
-
   return (
     <div className="content">
       <Grid container>
+        {isEmpty && (
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: '100vh' }}
+          >
+            {isLoading && !hasError && <CircularProgress />}
+            {hasError && <Alert severity="error">Error - Please try again later</Alert>}
+          </Grid>
+        )}
+
         <Grid item xs={12} md={7} sm={7} lg={5}>
-          {stores.map((store) => (
-            <StoreCard
-              key={store.id}
-              id={store.id}
-              name={store.name}
-              currentCapacity={store.currentCapacity}
-              maxCapacity={store.maxCapacity}
-              isFavorite={isFavorite(store.id)}
-            />
-          ))}
+          {!isEmpty &&
+            stores.map((store) => (
+              <StoreCard
+                key={store.id}
+                id={store.id}
+                name={store.name}
+                currentCapacity={store.currentCapacity}
+                maxCapacity={store.maxCapacity}
+                isFavorite={isFavorite(store.id)}
+              />
+            ))}
         </Grid>
       </Grid>
     </div>
