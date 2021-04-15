8c45c519eaccc4ef23285968513a435ddf2de57c
Jonas Hauser <jonas.hauser@ost.ch>
2021-04-15 21:45:26 +0200

    fix(title): app title and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { CssBaseline } from '@material-ui/core'
-import { createMuiTheme } from '@material-ui/core/styles'
-import { ThemeProvider } from '@material-ui/styles'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { CssBaseline } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { BrowserRouter } from 'react-router-dom';
 
-import React from 'react'
-import SearchAppBar from './components/AppBar'
-import Footer from './components/Footer'
-import AppRouter from './AppRouter'
+import React from 'react';
+import SearchAppBar from './components/AppBar';
+import Footer from './components/Footer';
+import AppRouter from './AppRouter';
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
         <Footer />
       </ThemeProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
-import SearchIcon from '@material-ui/icons/Search'
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -45,17 +45,17 @@ const useStyles = makeStyles((theme) => ({
   toolbarButtons: {
     marginLeft: 'auto'
   }
-}))
+}));
 
 export default function SearchAppBar() {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <Typography className={classes.title} variant="h6" noWrap>
-            Capwatch
+            CapWatch
           </Typography>
           <div className={classes.toolbarButtons}>
             <IconButton aria-label="search" color="inherit">
@@ -65,5 +65,5 @@ export default function SearchAppBar() {
         </Toolbar>
       </AppBar>
     </div>
-  )
+  );
 }
